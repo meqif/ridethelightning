3437db0e27088efbe284c167dbd94921752a2b0c
Ricardo Martins <ricardo@scarybox.net>
2014-09-20 19:44:27 +0100

    Split some code into another module.

--- app.js
+++ app.js
@@ -1,69 +1,24 @@
 var WebSocket = require('ws'),
     https = require('https'),
     _ = require('underscore'),
-    geo = require('geolib'),
     async = require('async'),
     express = require('express'),
     bodyParser = require('body-parser'),
-    morgan = require('morgan');
+    morgan = require('morgan'),
+    push = require('./lib/push');
+
 
 var remote = 'ws://ws.lightningmaps.org',
     ws = new WebSocket(remote);
 
 var subscribers = [];
 
-var sendStrike = function(token, strike) {
-    var req = https.request({
-        hostname: 'api.pushbullet.com',
-        port: 443,
-        path: '/v2/pushes',
-        method: 'POST',
-        auth: token + ':',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    }, function(res) {
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('BODY: ' + chunk);
-        });
-    });
-
-    req.write(JSON.stringify({
-        type: 'address',
-        title: 'Lightning strike',
-        address: strike.lat + ',' + strike.lon
-    }));
-
-    req.on('error', function(err) {
-        console.log(err);
-    });
-
-    req.end();
-};
-
-var sendStrikesToSubscriber = function(data) {
-    return function(subscriber) {
-        var inside = _.filter(data.strokes, function(stroke) {
-            return geo.isPointInCircle(
-                { latitude: stroke.lat, longitude: stroke.lon }, // stroke
-                { latitude: subscriber.latitude, longitude: subscriber.longitude }, // subscriber's centre
-                10000 // radius in metres
-            );
-        });
-
-        _.each(inside, function(strike) { sendStrike(subscriber.token, strike); });
-    };
-};
-
 ws.on('open', function() {});
 
 ws.on('message', function(message) {
     var data = JSON.parse(message);
 
-    async.each(subscribers, sendStrikesToSubscriber(data));
+    async.each(subscribers, push.sendStrikesToSubscriber(data));
 });
 
 var app = express();
