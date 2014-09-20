844a0a128d46d21d43f3528db5758380b9dfa862
Ricardo Martins <ricardo@scarybox.net>
2014-09-20 23:20:37 +0100

    Add more tests.

--- test/api.js
+++ test/api.js
@@ -18,9 +18,30 @@ describe('POST /api/v1/subscribe', function() {
             .expect('Content-Type', /json/)
             .expect(200, done);
     });
+
+    describe('with a populated subscriber list', function() {
+        request(app)
+            .post('/api/v1/subscribe')
+            .send({ token: 'foo', location: { latitude: 1, longitude: 1 } });
+
+        it('overwrites existing subscribers on resubscription', function(done) {
+            request(app)
+                .post('/api/v1/subscribe')
+                .send({ token: 'foo', location: { latitude: 1, longitude: 1 } })
+                .expect('Content-Type', /json/)
+                .expect(200, done);
+        });
+    });
 });
 
 describe('POST /api/v1/unsubscribe', function() {
+    it('refuses malformed requests', function(done) {
+        request(app)
+            .post('/api/v1/unsubscribe')
+            .send()
+            .expect(400, done);
+    });
+
     describe('with a populated subscriber list', function() {
         request(app)
             .post('/api/v1/subscribe')
