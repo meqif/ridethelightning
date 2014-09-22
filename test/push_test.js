3115305cdff713e9a40618cd9c99255b011532a2
Ricardo Martins <ricardo@scarybox.net>
2014-09-22 12:31:47 +0100

    Add test.

--- test/push_test.js
+++ test/push_test.js
@@ -3,7 +3,8 @@ var nock = require('nock');
 var push = require('../lib/push');
 
 describe('sendStrikesToSubscriber', function() {
-    var strikes = [{ lat: 1, lon: 2 }];
+    var strikesInRange = [{ lat: 1, lon: 2 }];
+    var strikesOutsideRange = [{ lat: 10, lon: 2 }];
     var subscriber = { token: 'foo', latitude: 1, longitude: 2 };
 
     before(function(done) {
@@ -16,16 +17,29 @@ describe('sendStrikesToSubscriber', function() {
         done();
     });
 
-    it('sends a notification per strike', function(done) {
-        nock.disableNetConnect();
+    it('sends a notification per strike if it is in range', function(done) {
         var scope = nock('https://api.pushbullet.com', {
             reqheaders: {
                 'Content-Type': 'application/json'
             }})
             .post('/v2/pushes')
             .reply(200);
-        push.sendStrikesToSubscriber({strokes: strikes}, subscriber);
+        push.sendStrikesToSubscriber({strokes: strikesInRange}, subscriber);
         scope.done();
         done();
+    });
+
+    it('does not send a notification if it is not in range', function(done) {
+        var scope = nock('https://api.pushbullet.com')
+            .post('/v2/pushes')
+            .reply(200);
+        push.sendStrikesToSubscriber({strokes: strikesOutsideRange}, subscriber);
+
+        if (!scope.isDone()) {
+            nock.cleanAll();
+            done();
+        } else {
+            throw new Error('Message sent when it should not');
+        }
     });
 });
